6700756fdb2b669603c1274dfe0cf52a84c3c025
jennypol <jenny.polla@edu.turkuamk.fi>
2022-12-08 15:07:30 +0200

    jotain

--- components/AddTransaction.js
+++ components/AddTransaction.js
@@ -30,10 +30,11 @@ export const AddTransaction = () => {
         <div className="form-control">
           <label htmlFor="amount"
             >Amount <br />
-            (negative - expense, positive - income)</label
-          >
-          <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
+            (negative - expense, positive - income)</label>
+            <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
         </div>
+
+
         <button className="btn">Add transaction</button>
       </form>
     </>
